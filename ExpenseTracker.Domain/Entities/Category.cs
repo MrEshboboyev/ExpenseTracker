340dbf1226b7459d253bba3d8e405dafb837cb4d
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-22 14:56:54 +0500

    Creating Models in Domain project: ApplicationUser.cs : inherit from IdentityUser and FullName property; Category.cs : Id, Name; Expense : Id, Description, Amount, Date, UserId(User), CategoryId(Category); Package added to Domain : Microsoft.AspNetCore.Identity.EntityFrameworkCore;

---
+++ ExpenseTracker.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Domain.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+}
---
+++ ExpenseTracker.Domain/Entities/Expense.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Domain.Entities
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than zero.")]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+    }
+}
