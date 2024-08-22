36f1490221bbd19991e0f915bd035a5da4dc2943
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-22 19:02:03 +0500

    Default CRUD Controllers Created for Category, Expense;

---
+++ ExpenseTracker.Web/Controllers/CategoryController.cs
@@ -0,0 +1,74 @@
+﻿using ExpenseTracker.Application.Services.Interfaces;
+using ExpenseTracker.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        // inject IExpenseService
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService expenseService)
+        {
+            _categoryService = expenseService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            var expenses = _categoryService.GetAllCategories();
+            return Ok(expenses);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            var category = _categoryService.GetCategoryById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory([FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _categoryService.CreateCategory(category);
+            return CreatedAtAction(nameof(GetCategoryById), new { id = category.Id }, category);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateCategory(int id, [FromBody] Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest("Id and Expense id must be equal!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _categoryService.UpdateCategory(category);
+
+            return NoContent(); 
+        }
+        
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteCategory(int id)
+        {
+            _categoryService.DeleteCategory(id);
+            return NoContent(); 
+        }
+    }
+}
---
+++ ExpenseTracker.Web/Controllers/ExpenseController.cs
@@ -0,0 +1,74 @@
+﻿using ExpenseTracker.Application.Services.Interfaces;
+using ExpenseTracker.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        // inject IExpenseService
+        private readonly IExpenseService _expenseService;
+
+        public ExpenseController(IExpenseService expenseService)
+        {
+            _expenseService = expenseService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllExpenses()
+        {
+            var expenses = _expenseService.GetAllExpenses();
+            return Ok(expenses);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetExpenseById(int id)
+        {
+            var expense = _expenseService.GetExpenseById(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            return Ok(expense);
+        }
+
+        [HttpPost]
+        public IActionResult CreateExpense([FromBody] Expense expense)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _expenseService.CreateExpense(expense);
+            return CreatedAtAction(nameof(GetExpenseById), new { id = expense.Id }, expense);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateExpense(int id, [FromBody] Expense expense)
+        {
+            if (id != expense.Id)
+            {
+                return BadRequest("Id and Expense id must be equal!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _expenseService.UpdateExpense(expense);
+
+            return NoContent(); 
+        }
+        
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteExpense(int id)
+        {
+            _expenseService.DeleteExpense(id);
+            return NoContent(); 
+        }
+    }
+}
