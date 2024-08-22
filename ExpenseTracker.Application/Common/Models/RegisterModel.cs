c735caf31c93cb4275bc5a8d0348e3f85e25eda5
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-22 20:19:39 +0500

    Models for Authentication : Application/Common/Models : LoginModel.cs : UserName, Password; RegisterModel.cs : UserName, Email, Password;

---
+++ ExpenseTracker.Application/Common/Models/RegisterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracker.Application.Common.Models
+{
+    public class RegisterModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
