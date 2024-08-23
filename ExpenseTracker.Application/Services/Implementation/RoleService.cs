1b5397ee269d2d9a8a46128ceae5c0d20b6a6455
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-23 21:07:48 +0500

    Application/Services/Interfaces : create IRoleService with CRUD; Application/Services/Implementation : create RoleService.cs inherit from IRoleService, inject RoleManager, methods implemented;

---
+++ ExpenseTracker.Application/Services/Implementation/RoleService.cs
@@ -0,0 +1,55 @@
+﻿using ExpenseTracker.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExpenseTracker.Application.Services.Implementation
+{
+    public class RoleService : IRoleService
+    {
+        // inject roleManager
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleService(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IdentityResult> CreateRoleAsync(string roleName)
+        {
+            return await _roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+
+        public async Task<IdentityResult> DeleteRoleAsync(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (role is not null) await _roleManager.DeleteAsync(role);
+            return IdentityResult.Failed(new IdentityError
+            {
+                Description = $"Role with ID '{roleId}' not found."
+            });
+        }
+
+        public async Task<IEnumerable<IdentityRole>> GetAllRolesAsync()
+        {
+            return _roleManager.Roles;
+        }
+
+        public async Task<IdentityRole> GetRoleByIdAsync(string roleId)
+        {
+            return await _roleManager.FindByIdAsync(roleId);
+        }
+
+        public async Task<IdentityResult> UpdateRoleAsync(string roleId, string newRoleName)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (role is not null)
+            {
+                role.Name = newRoleName;
+                await _roleManager.DeleteAsync(role);
+            }
+            return IdentityResult.Failed(new IdentityError
+            {
+                Description = $"Role with ID '{roleId}' not found."
+            });
+        }
+    }
+}
---
+++ ExpenseTracker.Application/Services/Interfaces/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ExpenseTracker.Application.Services.Interfaces
+{
+    public interface IRoleService
+    {
+        Task<IEnumerable<IdentityRole>> GetAllRolesAsync();
+        Task<IdentityRole> GetRoleByIdAsync(string roleId);
+        Task<IdentityResult> CreateRoleAsync(string roleName);
+        Task<IdentityResult> UpdateRoleAsync(string roleId, string newRoleName);
+        Task<IdentityResult> DeleteRoleAsync(string roleId);
+    }
+}
